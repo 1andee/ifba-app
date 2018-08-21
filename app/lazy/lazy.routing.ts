8d29975eb2d3aae56252229fe86dec7a403fe03b
Ian Duke <ian@thefarmventures.com>
2018-08-21 10:28:21 -0500

    locate component

--- app/lazy/lazy.routing.ts
+++ app/lazy/lazy.routing.ts
@@ -1,11 +1,13 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from "../pages/home/home.component";
+import { LocateComponent } from "../pages/locate/locate.component";
 import { ContactComponent } from "../pages/contact/contact.component";
 
 const routes: Routes = [
     { path: "home", component: HomeComponent },
     { path: "contact", component: ContactComponent },
+    { path: "locate", component: LocateComponent },
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forChild(routes);
@@ -13,4 +15,5 @@ export const routing: ModuleWithProviders = RouterModule.forChild(routes);
 export const navigatableComponents = [
     HomeComponent,
     ContactComponent,
+    LocateComponent,
 ];
--- app/pages/home/home.component.ts
+++ app/pages/home/home.component.ts
@@ -4,8 +4,6 @@ import { RouterExtensions } from "nativescript-angular/router";
 import * as utils from "utils/utils";
 import * as geolocation from "nativescript-geolocation";
 import { Accuracy } from "ui/enums"; // used to describe at what accuracy the location should be get
-import { ListPicker } from "ui/list-picker";
-import * as dialogs from "ui/dialogs";
 
 @Component({
     moduleId: module.id,
@@ -14,14 +12,10 @@ import * as dialogs from "ui/dialogs";
     styleUrls: ["./home-common.css", "./home.css"]
 })
 export class HomeComponent {
-    clubs: any = [];
-    filteredClubs: any = [];
     lat: any;
     lon: any;
     speed: any;
     addr: any;
-    stateSelector: any = [];
-    displayName: string;
 
     constructor(private page: Page, private router: RouterExtensions) {
         page.actionBarHidden = false;
@@ -30,66 +24,7 @@ export class HomeComponent {
         this.lon = "";
         this.speed = "";
         this.addr = "";
-        this.clubs = [
-            {
-                "name": "Box 43 Association",
-                "region": 10,
-                "country": "Canada",
-                "state": "Ontario",
-                "state_code": "ON",
-                "location": "Hamilton",
-                "twitter": "Box43Pio",
-                "canteen": true
-            },
-            {
-                "name": "Club Appel 99",
-                "region": 10,
-                "country": "Canada",
-                "state": "Quebec",
-                "state_code": "QC",
-                "location": "Quebec City",
-                "website": "http://www.clubappel99.ca",
-                "canteen": true
-            },
-            {
-                "name": "Greater Toronto Multiple Alarm Association",
-                "region": 10,
-                "country": "Canada",
-                "state": "Ontario",
-                "state_code": "ON",
-                "location": "Toronto",
-                "website": "gtmaa.com",
-                "facebook": "http://facebook.com/gtmaa",
-                "twitter": "gtmaa",
-                "instagram": "gtmaa.sup7",
-                "canteen": true
-            },
-            {
-                "name": "Pompiers Auxiliaires de Montreal",
-                "region": 10,
-                "country": "Canada",
-                "state": "Quebec",
-                "state_code": "QC",
-                "location": "Montreal",
-                "website": "http://www.museedespompiers.com",
-                "facebook": "https://www.facebook.com/museedespompiersdemontreal",
-                "canteen": true
-            },
-            {
-                "name": "Ontario Fire Buff Associates",
-                "region": 10,
-                "country": "Canada",
-                "state": "Ontario",
-                "state_code": "ON",
-                "website": "http://www.ofba.ca",
-                "facebook": "https://www.facebook.com/groups/169640076461/"
-            }
-        ];
-        this.stateSelector = [];
-        this.clubs.forEach((club) => {
-            this.stateSelector.push(club.state_code);
 
-        })
     }
 
     getLocation() {
@@ -113,24 +48,6 @@ export class HomeComponent {
     }
     goTwitter() {
         utils.openUrl("https://twitter.com/intfirebuff");
-    }
-    filterClubs(state) {
-        this.filteredClubs = [];
-        this.clubs.forEach((club) => {
-            if (state === 'all') {
-                this.filteredClubs.push(club);
-            } else if (club.state_code === state) {
-                this.filteredClubs.push(club);
-            }
-        })
-    }
-    // selectedIndexChanged(args) {
-    //     let picker = <ListPicker>args.object;
-    //     this.picked = this.stateSelector[picker.selectedIndex];
-    //     this.filterClubs(this.picked);
-    // }
-    ngOnInit() {
-        this.filterClubs('all');
     }
     goTo(route: string) {
         this.router.navigate(["/lazy/" + route], { clearHistory: false });
