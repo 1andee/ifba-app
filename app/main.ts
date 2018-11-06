faf487cebb4ee8d2a0c238d52d556f64d482e5b2
Ian Duke <hey@ianduke.io>
2018-11-06 09:49:43 -0500

    deps and livesync nav

--- app/main.ts
+++ app/main.ts
@@ -4,13 +4,15 @@ import { AppOptions } from "nativescript-angular/platform-common";
 
 import { AppModule } from "./app.module";
 
+// Attach livesync hooks for navigation
+import "./livesync-navigation";
+
 //  Hot Module Replacement
 let options: AppOptions = {};
 if (module['hot']) {
     const hmrUpdate = require("nativescript-dev-webpack/hmr").hmrUpdate;
 
-    // options.hmrOptions = {
-    options['hmrOptions'] = {
+    options.hmrOptions = {
         moduleTypeFactory: () => AppModule,
         livesyncCallback: (platformReboot) => {
             console.log("HMR: Sync...")
